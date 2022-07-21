c6b9abc79368f948668ccdadc61f9d0006ae3386
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-07-20 20:51:29 -0400

    Starting with Publishers and Subscribers.

--- 02-publishers-and-subscribers/projects/Starter.playground/Contents.swift
+++ 02-publishers-and-subscribers/projects/Starter.playground/Contents.swift
@@ -1,10 +1,114 @@
 import Foundation
 import Combine
-import _Concurrency
+//import _Concurrency
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+example(of: "Publisher") {
+	let myNotification = Notification.Name("MyNotification")
+//	 let publisher = NotificationCenter.default
+//		.publisher(for: myNotification, object: nil)
+	let center = NotificationCenter.default
+	let observer = center.addObserver(forName: myNotification, object: nil, queue: nil) { notification in
+		print("Received!")
+	}
+	center.post(name: myNotification, object: nil)
+	center.removeObserver(observer)
+}
+
+example(of: "Subscriber") {
+	let myNotification = Notification.Name("MyNotification")
+	let center = NotificationCenter.default
+	let publisher = center.publisher(for: myNotification, object: nil)
+	let subscription = publisher
+		.sink { _ in
+			print("Received!")
+		}
+	center.post(name: myNotification, object: nil)
+	subscription.cancel()
+}
+
+example(of: "Just") {
+	let just = Just("Hello world!")
+	_ = just
+		.sink(
+			receiveCompletion: {
+				print("Received completion", $0)
+			},
+			receiveValue: {
+				print("Received value", $0)
+			}
+		)
+	_ = just
+		.sink(
+			receiveCompletion: {
+				print("Received completion (another)", $0)
+			},
+			receiveValue: {
+				print("Received value (another)", $0)
+			}
+		)
+}
+
+example(of: "assign(to:on:)") {
+	class SomeObject {
+		var value: String = "" {
+			didSet {
+				print(value)
+			}
+		}
+	}
+	let object = SomeObject()
+	let publisher = ["Hello", "world!"].publisher
+	_ = publisher
+		.assign(to: \.value, on: object)
+}
+
+example(of: "assign(to:)") {
+	class SomeObject {
+		@Published var value = -1
+	}
+	let object = SomeObject()
+	object.$value
+		.sink {
+			print("1", $0)
+		}
+	object.$value
+		.sink {
+			print("2", $0)
+		}
+	(0..<10).publisher
+		.assign(to: &object.$value) // No AnyCancellable
+	object.value = 100
+}
+
+example(of: "Custom Subscriber") {
+	let publisher = (1...6).publisher
+	// FAILS let publisher = ["A", "B", "C", "D", "E", "F"].publisher
+
+	final class IntSubscriber: Subscriber {
+		typealias Input = Int
+		typealias Failure = Never
+
+		func receive(subscription: Subscription) {
+			subscription.request(.max(3))
+		}
+		
+		func receive(_ input: Int) -> Subscribers.Demand {
+			print("Received value", input)
+			//return .none // Continue with 3
+			//return .unlimited
+			return .max(1)
+		}
+		
+		func receive(completion: Subscribers.Completion<Never>) {
+			print("Received completion", completion)
+		}
+	}
+	
+	let subscriber = IntSubscriber()
+	publisher.subscribe(subscriber)
+}
 
 /// Copyright (c) 2021 Razeware LLC
 ///
