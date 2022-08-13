29341e98c6c46e707c1728d89a353d9f7e2a5c0f
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-13 17:48:36 -0400

    Added flatMap operator.

--- 03-transforming-operators/projects/Starter.playground/Contents.swift
+++ 03-transforming-operators/projects/Starter.playground/Contents.swift
@@ -49,6 +49,28 @@ example(of: "tryMap") {
 		.store(in: &subscriptions)
 }
 
+example(of: "flatMap") {
+	func decode(_ codes: [Int]) -> AnyPublisher<String, Never> {
+		Just(
+			codes
+				.compactMap { code in
+					guard (32...255).contains(code) else { return nil }
+					return String(UnicodeScalar(code) ?? " ")
+				}
+				.joined()
+		)
+		.eraseToAnyPublisher()
+	}
+	
+	[72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33]
+		.publisher
+		.collect()
+		.flatMap(decode)
+		.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+}
+
 /// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
