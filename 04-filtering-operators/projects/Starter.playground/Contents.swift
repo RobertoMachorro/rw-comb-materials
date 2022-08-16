aa60ca5a732574fe81ea21b3b7bc2b699d42cbdb
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-16 15:32:04 -0400

    Added drop(while:) filter operator.

--- 04-filtering-operators/projects/Starter.playground/Contents.swift
+++ 04-filtering-operators/projects/Starter.playground/Contents.swift
@@ -92,6 +92,14 @@ example(of: "dropFirst") {
 		.store(in: &subscriptions)
 }
 
+example(of: "drop(while:)") {
+	let numbers = (1...10).publisher
+	numbers
+		.drop(while: { $0 % 5 != 0 })
+		.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+}
+
 /// Copyright (c) 2021 Razeware LLC
 ///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
