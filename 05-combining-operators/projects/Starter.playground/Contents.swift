29640d40fdff93a1ec897bad692817f48643d4de
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-17 17:20:07 -0400

    Added prepend(Publisher) with PassthroughSubject in combine op.

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -31,6 +31,20 @@ example(of: "prepend(Publisher)") {
 		.store(in: &subscriptions)
 }
 
+example(of: "prepend(Publisher) #2") {
+	let publisher1 = [3, 4].publisher
+	let publisher2 = PassthroughSubject<Int, Never>()
+	
+	publisher1
+		.prepend(publisher2)
+		.sink(receiveValue: { print($0) })
+		.store(in: &subscriptions)
+	
+	publisher2.send(1)
+	publisher2.send(2)
+	publisher2.send(completion: .finished)
+}
+
 // Copyright (c) 2021 Razeware LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
