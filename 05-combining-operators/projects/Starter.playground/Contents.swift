84a3960d35f3b3a032c7887602d3d043c0fcadee
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-22 21:56:41 -0400

    Added switchToLatest() operation to flatten a stream of publishers.

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -88,6 +88,40 @@ example(of: "append(Publisher)") {
 		.store(in: &subscriptions)
 }
 
+example(of: "switchToLatest") {
+	let publisher1 = PassthroughSubject<Int, Never>()
+	let publisher2 = PassthroughSubject<Int, Never>()
+	let publisher3 = PassthroughSubject<Int, Never>()
+	
+	let publishers = PassthroughSubject<PassthroughSubject<Int, Never>, Never>()
+	
+	publishers
+		.switchToLatest()
+		.sink(
+			receiveCompletion: { _ in print("Completed!") },
+			receiveValue: { print($0) }
+		)
+		.store(in: &subscriptions)
+
+	publishers.send(publisher1)
+	publisher1.send(1)
+	publisher1.send(2)
+	
+	publishers.send(publisher2)
+	publisher1.send(3)
+	publisher2.send(4)
+	publisher2.send(5)
+	
+	publishers.send(publisher3)
+	publisher2.send(6)
+	publisher3.send(7)
+	publisher3.send(8)
+	publisher3.send(9)
+	
+	publisher3.send(completion: .finished)
+	publishers.send(completion: .finished)
+}
+
 // Copyright (c) 2021 Razeware LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
