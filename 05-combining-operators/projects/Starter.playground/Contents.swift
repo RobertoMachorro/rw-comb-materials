47e11ccb34f1d044b1dc145e9b9efde2149ae62a
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-28 09:19:56 -0400

    Added merge(with:) combining operator.

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -154,6 +154,31 @@ example(of: "switchToLatest - Network Request") {
 }
 */
 
+example(of: "merge(with:)") {
+	let publisher1 = PassthroughSubject<Int, Never>()
+	let publisher2 = PassthroughSubject<Int, Never>()
+	
+	publisher1
+		.merge(with: publisher2)
+		.sink(
+			receiveCompletion: { _ in print("Completed") },
+			receiveValue: { print($0) }
+		)
+		.store(in: &subscriptions)
+
+	publisher1.send(1)
+	publisher1.send(2)
+	
+	publisher2.send(3)
+	
+	publisher1.send(4)
+	
+	publisher2.send(5)
+	
+	publisher1.send(completion: .finished)
+	publisher2.send(completion: .finished)
+}
+
 // Copyright (c) 2021 Razeware LLC
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
