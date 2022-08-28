724c88e149cd95fdc0758d0799322b745f6987d5
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-08-28 09:43:45 -0400

    Added combineLatest combining operator.

--- 05-combining-operators/projects/Starter.playground/Contents.swift
+++ 05-combining-operators/projects/Starter.playground/Contents.swift
@@ -165,7 +165,7 @@ example(of: "merge(with:)") {
 			receiveValue: { print($0) }
 		)
 		.store(in: &subscriptions)
-
+	
 	publisher1.send(1)
 	publisher1.send(2)
 	
@@ -175,6 +175,32 @@ example(of: "merge(with:)") {
 	
 	publisher2.send(5)
 	
+	publisher1.send(completion: .finished)
+	publisher2.send(completion: .finished)
+}
+
+example(of: "combineLatest") {
+	let publisher1 = PassthroughSubject<Int, Never>()
+	let publisher2 = PassthroughSubject<String, Never>()
+	
+	publisher1
+		.combineLatest(publisher2)
+		.sink(
+			receiveCompletion: { _ in print("Completed") },
+			receiveValue: { print("P1: \($0), P2: \($1)") }
+		)
+		.store(in: &subscriptions)
+
+	publisher1.send(1)
+	publisher1.send(2)
+
+	publisher2.send("a")
+	publisher2.send("b")
+
+	publisher1.send(3)
+
+	publisher2.send("c")
+
 	publisher1.send(completion: .finished)
 	publisher2.send(completion: .finished)
 }
